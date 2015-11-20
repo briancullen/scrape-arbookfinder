02b263054832877cac032417d4c9511edf718fd0
Brian Cullen <brian.cullen@gmail.com>
2015-11-20 23:58:33 +0000

    Implemented proper ISBN checks.
    Function not removes everything but digits and the character X (allowed
    in ISBN10). Checks if it is a ISBN10 and if it is converts it to a
    ISBN13. Return values as before - null on error.

--- core/util/isbn-util.js
+++ core/util/isbn-util.js
@@ -1,19 +1,102 @@
-
 // This function will check the provided ISBN
 // to make sure it is valid and remove any
 // hypens etc to make sure that it is just a
 // 13 digit number. Returns null if any of the
 // checks fail.
-module.exports.normaliseISBN = function (isbn) {
-    
-    // Check if it is already null
-    if (!isbn)
-        return isbn;
-    
-    // TODO: Implement more checks.
-    // Remove any hypens and trailing white spaces.
-	isbn = isbn.replace(/-/g, "").trim();
-    
+function normaliseISBN (isbn) {
+    isbn = isbn.replace(/[^\dX]/gi, '');
+    if (isISBN10(isbn)) {
+        isbn = convertToISBN13(isbn);
+    }
+    else if (!isISBN13(isbn)) {
+        return null;
+    }
+
     return isbn;
 }
+
+function convertToISBN13 (isbn) {
+    if(isbn.length != 10) {
+        return null;
+    }
+    
+    isbn = "978" + isbn.slice(0, -1);
+    var checkDigit = calculateISBN13CheckDigit(isbn);
+    return isbn + checkDigit.toString();
+}
+
+function calculateISBN13CheckDigit (isbn) {
+    if(isbn.length == 13) {
+        isbn = isbn.slice(0, -1);
+    }
+    
+    if (isbn.length != 12) {
+        return null;
+    }
+    
+    sum = 0;
+    for (var index = 0; index < isbn.length; index++) {
+        if (index % 2 == 1) {
+            sum += 3*parseInt(isbn[index]);
+        }
+        else {
+            sum += parseInt(isbn[index])
+        }
+    }
+    
+    var checkDigit = 10 - (sum % 10);
+    if (checkDigit == 10)
+        checkDigit = 0;
+    
+    return checkDigit;
+}
+
+function isISBN13 (isbn) {
+    if (isbn.length != 13) {
+        return false;
+    }
+    
+    return (calculateISBN13CheckDigit(isbn) == isbn[12]);
+}
+
+function isISBN10 (isbn) {
+    if(isbn.length != 10) {
+        return false;
+    }
+
+    var chars = isbn.split('');
+    if(chars[9].toUpperCase() == 'X') {
+        chars[9] = 10;
+    }
+    
+    var sum = 0;
+    for (var i = 0; i < chars.length; i++) {
+        sum += ((10-i) * parseInt(chars[i]));
+    }
+    
+    return ((sum % 11) == 0);
+}
+
+
+function isISBN10 (isbn) {
+    if(isbn.length != 10) {
+        return false;
+    }
+
+    var chars = isbn.split('');
+    if(chars[9].toUpperCase() == 'X') {
+        chars[9] = 10;
+    }
+    
+    var sum = 0;
+    for (var i = 0; i < chars.length; i++) {
+        sum += ((10-i) * parseInt(chars[i]));
+    }
+    
+    return ((sum % 11) == 0);
+}
+
+module.exports.normaliseISBN = normaliseISBN;
+
+
 
