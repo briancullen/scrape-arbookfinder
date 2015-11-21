bc2224f9f7bbd96a3792e7fee732f551aefb191d
Brian Cullen <brian.cullen@gmail.com>
2015-11-21 20:13:45 +0000

    Working HTTP-JSON version
    Currently hardwired to port 8080 but the code does work. URLs should be
    in the form of http://localhost:8080/isbn (where isbn is replaced with
    the isbn desired). Invalid characters in the path result in a 401 error
    while invalid ISBNs or failures to load pages will result in 404.

--- http-json/arfinder-http-json.js
+++ http-json/arfinder-http-json.js
@@ -1,28 +1,32 @@
 var core = require('../core/arfinder-core');
-var system = require('system');
+var webserver = require('webserver');
 
-function doSearch () {   
-    // Remove any hypens and trailing white spaces.
-	isbn = system.stdin.readLine();
-    
-    if (isbn == "")
+var server = webserver.create();
+console.log("Server created.")
+var service = server.listen(8080, function(request, response) {
+
+    var isbn = request.url.slice(1);
+    if (request.method != "GET" ||
+       !isbn.match(/[\dX-]/i))
     {
-        bookDB.close();
-        unknownBookDB.close();
-        core.endSearch();	        
+        response.statusCode = 401;
+        response.close();
+        return;
     }
     
-    core.searchByISBN(isbn, function(result) {
+    core.searchByISBN(isbn, function (result) {
+        
         if (result) {
-            print(JSON.stringify(result));
+            response.statusCode = 200;
+            response.setHeader("Content-Type", "text/json");
+            response.write(JSON.stringify(result));
+            response.close();
         }
         else {
-            print("{ error: 'Unable to process request' }")
+            response.statusCode = 404;
+            response.setHeader("Content-Type", "text/html");
+            response.write("<h1>Resource Not Found</h1>");
+            response.close();
         }
-        
-        doSearch();
-            
     });
-}
-
-core.initSearch(doSearch);
+});
