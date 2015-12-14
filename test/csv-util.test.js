04ca8415c4da3e590665afa317cc92b6fda9e4ef
Brian Cullen <brian.cullen@gmail.com>
2015-12-14 22:15:14 +0000

    Start of some proper testing using mocha - run npm test

---
+++ test/csv-util.test.js
@@ -0,0 +1,8 @@
+var test = require('unit.js');
+var objectutil = require("../core/util/object-util");
+
+describe('Testing CSV Util', function(){
+  describe('JSON to CSV', function(){
+    
+  });
+});
